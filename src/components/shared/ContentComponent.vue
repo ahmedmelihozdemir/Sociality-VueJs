db8a33e4ce7477631b088c247175633be13f82e0
Melih Özdemir <ozdemirmelihahmed@gmail.com>
2022-09-26 23:32:33 +0300

    edit: content comp.

--- src/components/shared/ContentComponent.vue
+++ src/components/shared/ContentComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="content-container">
+    <main class="content-container">
         <!-- <v-card class="content">
             <v-spacer></v-spacer>
             <v-container fluid>
@@ -36,13 +36,13 @@
 
         <v-card
             class="content"
-            min-width="440px"
-            max-width="440px"
+            min-width="550px"
+            max-width="360px"
             cols="12"
             md="6"
         >
             <v-spacer></v-spacer>
-            <v-container fluid>
+            <v-container fluid class="container-container">
                 <v-row class="content-row" dense>
                     <v-col
                         class="content-col"
@@ -50,6 +50,7 @@
                         :key="idx"
                         cols="12"
                         md="12"
+                        sm="12"
                     >
                         <v-card-actions class="content-upper">
                             <v-spacer class="content-published">
@@ -61,10 +62,12 @@
                             <v-btn class="btn-icon" icon>
                                 <v-icon>mdi-delete</v-icon>
                             </v-btn>
-                            <v-btn class="btn-icon" icon @click="moreInfo = !moreInfo">
+                            <v-btn class="btn-icon" icon @click="changeInfo">
                                 <v-icon>mdi-dots-horizontal</v-icon>
-                                {{ moreInfo }}
                             </v-btn>
+                            <!-- <ContentComponentInfo
+                                :moreInfo="info"
+                            ></ContentComponentInfo> -->
                         </v-card-actions>
                         <div class="content-card">
                             <div
@@ -77,13 +80,18 @@
                             >
                                 {{ i.account.channel }}
                             </div>
+
                             <v-img
-                                :src="i.entry.image"
+                                :src="i.entry.image[0]"  @error="$event.target.src='errorImage'"
                                 class="content-img white--text align-end"
                                 gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                                max-height="200px"
-                                max-width="360px"
+                                height="400px"
+                                max-width="470px"
                             >
+                                <ContentComponentInfo
+                                    :moreInfo="info"
+                                ></ContentComponentInfo>
+
                                 <v-card-title
                                     class="card-title"
                                     v-text="i.entry.message"
@@ -118,11 +126,89 @@
                         <v-divider class="my-2"></v-divider>
                     </v-col>
                 </v-row>
+
+                <v-row class="content-row" dense>
+                    <v-col
+                        class="content-col"
+                        v-for="(i, inf,idx) in getData['2021-07-01']"
+                        :key="idx"
+                        cols="12"
+                        md="12"
+                        sm="12"
+                    >
+                        <v-card-actions class="content-upper">
+                            <v-spacer class="content-published">
+                                {{ i.published_at }}
+                            </v-spacer>
+                            <v-btn class="btn-icon" icon>
+                                <v-icon>mdi-cancel</v-icon>
+                            </v-btn>
+                            <v-btn class="btn-icon" icon>
+                                <v-icon>mdi-delete</v-icon>
+                            </v-btn>
+                            <v-btn class="btn-icon" icon @click="info = !info">
+                                <v-icon>mdi-dots-horizontal</v-icon>
+                                {{ info }}
+                            </v-btn>
+                        </v-card-actions>
+                        <div class="content-card">
+                            <div
+                                class="content-channel"
+                                :class="[
+                                    i.account.channel == 'facebook'
+                                        ? 'channelColor1'
+                                        : 'channelColor2',
+                                ]"
+                            >
+                                {{ i.account.channel }}
+                            </div>
+                            <v-img
+                                :src="i.entry.image[0]"
+                                class="content-img white--text align-end"
+                                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                                height="400px"
+                                max-width="470px"
+                            >
+                                <v-card-title
+                                    class="card-title"
+                                    v-text="i.entry.message"
+                                ></v-card-title>
+                            </v-img>
+                        </div>
+                        <v-card-actions class="card-action">
+                            <v-spacer>
+                                <a
+                                    :href="i.link"
+                                    class="spacer-link card-spacer"
+                                    data-text="Go link"
+                                >
+                                    <a :href="i.link" class="spana"
+                                        >Come here!</a
+                                    >
+                                </a>
+                            </v-spacer>
+                            <v-btn class="btn-icon" icon>
+                                <v-icon>mdi-heart</v-icon
+                                ><span class="btn-icon-counter">0</span>
+                            </v-btn>
+                            <v-btn class="btn-icon" icon>
+                                <v-icon>mdi-bookmark</v-icon
+                                ><span class="btn-icon-counter">0</span>
+                            </v-btn>
+                            <v-btn class="btn-icon" icon>
+                                <v-icon>mdi-share-variant</v-icon
+                                ><span class="btn-icon-counter">0</span>
+                            </v-btn>
+                        </v-card-actions>
+                        <v-divider class="my-2"></v-divider>
+                        <div>{{i.entry.images}}</div>
+                    </v-col>
+                </v-row>
+
+
             </v-container>
         </v-card>
-        <!-- <div>{{ getData["2021-06-17"][0].entry.image }}</div> -->
-        <ContentComponentInfo :moreInfo="moreInfo"></ContentComponentInfo>
-    </div>
+    </main>
 </template>
 
 <script lang="ts">
@@ -131,39 +217,28 @@ import Component from "vue-class-component";
 import { Prop } from "vue-property-decorator";
 import datas from "@/data.json";
 import ContentComponentInfo from "./ContentComponentInfo.vue";
+import ContentComponentSecond from "./ContentComponentSecond.vue";
 
 @Component({
     name: "ContentComponent",
     components: {
         ContentComponentInfo,
+        ContentComponentSecond,
     },
 })
 export default class ContentComponent extends Vue {
-    cards = [
-        {
-            title: "Pre-fab homes",
-            src: "https://cdn.vuetifyjs.com/images/cards/house.jpg",
-            flex: 12,
-        },
-        {
-            title: "Favorite road trips",
-            src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
-            flex: 12,
-        },
-        {
-            title: "Best airlines",
-            src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
-            flex: 12,
-        },
-    ];
-    
-    moreInfo = false;
+    info = false;
     delete = false;
     like = false;
     save = false;
     share = false;
 
+    changeInfo() {
+        this.info = !this.info;
+    }
+
     getData = datas.posts_by_date;
+    errorImage = 'nopostimage.png';
     dataFirst = datas.posts_by_date["2021-06-17"];
     dataSecond = datas.posts_by_date["2021-07-01"];
 
@@ -190,8 +265,8 @@ export default class ContentComponent extends Vue {
         console.log(this.getData[20210617][1].entry.image);
     }
 
-    alert(){
-        alert("test")
+    alert() {
+        alert("test");
     }
 }
 </script>
---
+++ src/components/shared/ContentComponentInfo.vue
@@ -0,0 +1,82 @@
+<template>
+    <v-card class="moreInfo">
+        <v-card elevation="12" width="200" height="100">
+            <v-navigation-drawer floating permanent class="info-drawer">
+                <v-list dense rounded class="info-list">
+                    <v-list-item
+                        class="info-list-item"
+                        v-for="item in items"
+                        :key="item.title"
+                        link
+                    >
+                        <v-list-item-content class="info-list-item-contents">
+                            <v-list-item-title class="info-list-item-content">{{
+                                item.title
+                            }}</v-list-item-title>
+                        </v-list-item-content>
+                        <v-list-item-icon class="info-list-item-icons">
+                            <v-icon class="info-list-item-icon">{{
+                                item.icon
+                            }}</v-icon>
+                        </v-list-item-icon>
+                    </v-list-item>
+                    <div>=={{moreInfo}}</div>
+                </v-list>
+            </v-navigation-drawer>
+        </v-card>
+    </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop, Watch } from "vue-property-decorator";
+
+export default class ContentComponentInfo extends Vue {
+    @Prop() moreInfo!: boolean; 
+    /* moreInfo = true; */
+    items = [
+        { title: "Home", icon: "mdi-cog" },
+        { title: "About", icon: "mdi-forum" },
+    ];
+}
+</script>
+
+<style lang="scss" scoped>
+.moreInfo {
+    position: absolute;
+    /* top: 65px;
+    right: 23px; */
+    top: 0;
+    right: 0;
+    z-index: 999;
+    height: auto;
+    width: auto;
+    /* overflow: auto; */
+    .info-drawer {
+        background-color: transparent;
+
+        .info-list {
+            background-color: transparent;
+
+            .info-list-item {
+                display: flex;
+                flex-direction: row;
+                justify-content: start;
+                align-items: center;
+
+                .info-list-item-icons {
+                    .info-list-item-icon {
+                        font-size: 1.2rem;
+                    }
+                }
+                .info-list-item-contents {
+                    .info-list-item-content {
+                        font-size: 0.8rem;
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
